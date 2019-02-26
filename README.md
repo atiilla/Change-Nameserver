dfa02aaae3461908b24f33a482a4d2a7129ca445
injecti0n <rootgebruiker@gmaill.com>
2019-02-26 11:33:48 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Change-Nameserver
---
+++ changeNameServer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+rm -rf /etc/resolv.conf && \
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+echo "Well done! you have access everywhere now"
+echo -n $'\E[31m'
+echo $''
+echo $'      _,.'
+echo $'    ,` -.)'
+echo $'   \'( _/\'-\\\\-.'
+echo $'  /,|`--._,-^|          ,'
+echo $'  \\_| |`-._/||          ,\'|'
+echo $'    |  `-, / |         /  /'
+echo $'    |     || |        /  /'
+echo $'     `r-._||/   __   /  /'
+echo $' __,-<_     )`-/  `./  /'
+echo $'\'  \\   `---\'   \\   /  /'
+echo $'    |           |./  /'
+echo $'    /           //  /'
+echo $'\\_/\' \\         |/  /'
+echo $' |    |   _,^-\'/  /'
+echo $' |    , ``  (\\/  /_'
+echo $'  \\,.->._    \\X-=/^'
+echo $'  (  /   `-._//^`'
+echo $'   `Y-.____(__}'
+echo $'    |     {__)'
+echo $'          ()`'
+
